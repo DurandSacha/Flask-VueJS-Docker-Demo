76eb58551858d767af75a8c8fdfd33cd42a95d63
DurandSacha <sacha6623@gmail.com>
2020-11-15 23:52:19 +0100

    init structure

---
+++ webapp/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/strongly-recommended'
+  ],
+  rules: {
+    'indent': 'off',
+    'vue/script-indent': ['warn', 2, {
+    'baseIndent': 1
+    }]
+  },
+  "overrides": [
+    {
+      "files": ["*.vue","*.js"],
+      "rules": {
+        "indent": "off"
+      }
+    }
+  ]
+};
---
+++ webapp/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <p>{{ greeting }}</p>
+    <p>{{ flaskGreeting }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function(){
+        return {
+            greeting: 'Hello from Vue!',
+            flaskGreeting: ''
+        }
+    },
+  created: async function(){
+        const gResponse = await fetch("http://localhost:5000/greeting");   /* API adress with port*/
+        const gObject = await gResponse.json();
+        this.flaskGreeting = gObject.greeting;
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
